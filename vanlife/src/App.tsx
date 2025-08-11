44e3308c0cad3740211a7efd4438a908003ea3a8
Shema <shemaphilbert8@gmail.com>
2025-08-11 11:48:00 +0200

    Created my first link - using <Link>

--- vanlife/src/App.tsx
+++ vanlife/src/App.tsx
@@ -1,9 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route,Link } from "react-router-dom"
 import Home from './components/Home'
 import About from "./components/About"
 function App() {
   return (
     <BrowserRouter>
+    <nav className="flex justify-center items-center bg-white py-4 px-6">
+      <Link className="text-gray-600 hover:text-gray-900 font-medium py-2 px-4" to='/'>Home</Link>
+      <Link className="text-gray-600 hover:text-gray-900 font-medium py-2 px-4" to='/about'>About</Link>
+    </nav>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
