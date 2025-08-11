717fed69519d3a1b7c04d975330a31ca1eb7d4f5
Shema <shemaphilbert8@gmail.com>
2025-08-11 11:23:54 +0200

    refined project folder structure

--- vanlife/src/App.tsx
+++ vanlife/src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './components/Home'
+import About from "./components/About"
 function App() {
   return (
-    <>
-      <h1 className="bg-yellow-400">Hello React Router</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
 export default App
-        
---
+++ vanlife/src/components/Home.tsx
@@ -0,0 +1,8 @@
+
+function Home() {
+  return (
+    <div>Hello React Router</div>
+  )
+}
+
+export default Home
