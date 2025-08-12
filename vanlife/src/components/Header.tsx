b577831240bc0d2bb99d1c973c10476a6a17829c
Shema <shemaphilbert8@gmail.com>
2025-08-12 09:32:55 +0200

    changed react router version

---
+++ vanlife/src/components/Header.tsx
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <header className="bg-gray-100">
+  <nav className="max-w-7xl mx-auto flex justify-between items-center py-4 px-6">
+
+    <div className="text-2xl font-bold text-gray-900">
+      #VANLIFE
+    </div>
+
+   
+    <div className="flex space-x-6">
+      <a href="#" className="text-gray-700 hover:text-gray-900 font-medium">About</a>
+      <a href="#" className="text-gray-700 hover:text-gray-900 font-medium">Vans</a>
+    </div>
+  </nav>
+</header>
+
+  )
+}
+
+export default Header
