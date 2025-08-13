56f3790888819d28b5ec2983b5b673308caab334
Shema <shemaphilbert8@gmail.com>
2025-08-13 19:15:16 +0200

    Created Host vans page

--- vanlife/src/components/HostLayout.tsx
+++ vanlife/src/components/HostLayout.tsx
@@ -1,13 +1,43 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 
 const HostLayout = () => {
   return (
     <>
         <nav>
-            <Link className='px-7' to="/host">Dashboard</Link>
-            <Link className='px-7' to="/host/reviews">Reviews</Link>
-            <Link className='px-7' to="/host/income">Income</Link>
+            <NavLink
+                className={({ isActive }) =>
+                    `px-7${isActive ? ' text-blue-600 font-bold' : ''}`
+                }
+                end
+                to="/host"
+            >
+                Dashboard
+            </NavLink>
+            <NavLink
+                className={({ isActive }) =>
+                    `px-7${isActive ? ' text-blue-600 font-bold' : ''}`
+                }
+                to="/host/income"
+            >
+                Income
+            </NavLink>
+            <NavLink
+                className={({ isActive }) =>
+                    `px-7${isActive ? ' text-blue-600 font-bold' : ''}`
+                }
+                to="/host/vans"
+            >
+                vans
+            </NavLink>
+            <NavLink
+                className={({ isActive }) =>
+                    `px-7${isActive ? ' text-blue-600 font-bold' : ''}`
+                }
+                to="/host/reviews"
+            >
+                Reviews
+            </NavLink>
             <Outlet />
         </nav>
     </>
